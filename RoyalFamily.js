760114f8507297012450a2accd4b00c4c7055082
BluFox2003 <matthew.reynard03@outlook.com>
2021-10-13 10:25:42 +0100

    Update RoyalFamily.js

--- RoyalFamily.js
+++ RoyalFamily.js
@@ -4,21 +4,37 @@ const GeorgeVI = {
 const Lizzy2 = {
     name: "Elizabeth II"
 }
-Lizzy2.parents = [GeorgeVI, "Elizabeth"]
+Lizzy2.parents = [GeorgeVI.name, "Elizabeth"]
 
 const PrincessMargaret = {
     name: "Margaret"
 }
-PrincessMargaret.parents = [GeorgeVI, "Elizabeth"]
+
+PrincessMargaret.parents = [GeorgeVI.name, "Elizabeth"]
+
 
 const Philip = {
     name: "Philip"
 }
 
+const POW = {
+    name: "Charles"
+}
+
+const Diana = {
+    name: "Diana"
+}
+
+const Camila = {
+    name: "Camila"
+}
+
+POW.parents = [Philip, Lizzy2]
+
 function childOf(Object){
     return Object.parents;
 }
 
-console.log(childOf(Lizzy2))
+console.log(childOf(POW))
 
 //GitHub Test
