1aa27b5653a9379e73f58c8adb9b329a92a7a3a0
BluFox2003 <matthew.reynard03@outlook.com>
2021-10-13 10:09:26 +0100

    Added RoyalFamily JS file

---
+++ RoyalFamily.js
@@ -0,0 +1,22 @@
+const GeorgeVI = {
+    name: "George VI"
+}
+const Lizzy2 = {
+    name: "Elizabeth II"
+}
+Lizzy2.parents = [GeorgeVI, "Elizabeth"]
+
+const PrincessMargaret = {
+    name: "Margaret"
+}
+PrincessMargaret.parents = [GeorgeVI, "Elizabeth"]
+
+const Philip = {
+    name: "Philip"
+}
+
+function childOf(Object){
+    return Object.parents;
+}
+
+console.log(childOf(Lizzy2))
