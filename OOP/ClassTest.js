6941048721dfd11d88e325443be6c658582ced01
BluFox2003 <matthew.reynard03@outlook.com>
2021-10-14 10:04:50 +0100

    Create ClassTest.js

---
+++ OOP/ClassTest.js
@@ -0,0 +1,10 @@
+class Cars {
+    constructor(manufacturer, colour) {
+        this.manufacturer = manufacturer
+        this.colour = colour
+    }
+}
+
+carOne = new Cars ("Ford", "Blue");
+
+console.log(carOne)
