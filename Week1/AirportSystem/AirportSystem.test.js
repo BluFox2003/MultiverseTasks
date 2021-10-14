45885ab2045e14fb2fc114acc1240ce973e341ea
BluFox2003 <matthew.reynard03@outlook.com>
2021-10-14 12:16:03 +0100

    split the tests up

--- Week1/AirportSystem/AirportSystem.test.js
+++ Week1/AirportSystem/AirportSystem.test.js
@@ -2,14 +2,31 @@ const {Passenger} = require("./Passenger")
 const {Bag} = require("./Bag")
 
 
-
-describe("AirportTest", () => {
+describe("PassengerTests", () => {
     beforeAll(() => {
         P1 = new Passenger("Joe", 69420, "15C")
     })
     test("Should return if Passenger info is in an array", () => {
         expect(typeof P1).toBe("object")
     })
+    test("Should return the name given", () => {
+        expect(P1.name).toBe("Joe")
+    })
+    test("Should return the name as a string", () => {
+        expect(typeof P1.name).toBe("string")
+    })
+    test("Should return flight number as an integer", () => {
+        expect(typeof P1.flightNumber).toBe("number")
+    })
+    test("Should return the seat number given", () => {
+        expect(P1.seatNumber).toBe("15C")
+    })
+    test("Should return seat number as a string", () => {
+        expect(typeof P1.seatNumber).toBe("string")
+    })
+})
+
+describe("BagTests", () => {
     test("Should return a number from bag weight", () => {
         expect(typeof new Bag(12).weight).toBe("number")
     })
