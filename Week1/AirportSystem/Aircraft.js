4923e0d33ecb318130370268f25b41e3fdf64776
BluFox2003 <matthew.reynard03@outlook.com>
2021-10-14 14:06:17 +0100

    :)

---
+++ Week1/AirportSystem/Aircraft.js
@@ -0,0 +1,10 @@
+class Aircraft {
+    constructor(model, registration, capacity, company){
+        this.model = model
+        this.registration = registration
+        this.capacity = capacity
+        this.company = company
+    }
+}
+
+P1 = new Aircraft("Boeing 737", "73G", 188, "British Airways")
