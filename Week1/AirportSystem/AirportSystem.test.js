da0e3b6a28c71723b33fd7bc47a15ec19b237975
BluFox2003 <matthew.reynard03@outlook.com>
2021-10-14 11:21:41 +0100

    Update AirportSystem.test.js

--- Week1/AirportSystem/AirportSystem.test.js
+++ Week1/AirportSystem/AirportSystem.test.js
@@ -7,4 +7,7 @@ describe("AirportTest", () => {
     test("Should return a number from bag weight", () => {
         expect(typeof new Bag(12).weight).toBe("number")
     })
+    test("Should return the exact same number weight as passed", () =>{
+        expect(new Bag(15).weight).toEqual(15)
+    })
 })
