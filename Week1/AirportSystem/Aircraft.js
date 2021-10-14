d28915a71d34e55d63bcf19e04167c7e4e615e26
BluFox2003 <matthew.reynard03@outlook.com>
2021-10-14 14:30:04 +0100

    Added Arrival and Departure to Aircraft

--- Week1/AirportSystem/Aircraft.js
+++ Week1/AirportSystem/Aircraft.js
@@ -1,13 +1,27 @@
+const {Airport} = require("./Airport")
+
 class Aircraft {
     constructor(model, registration, capacity, company){
         this.model = model
         this.registration = registration
         this.capacity = capacity
         this.company = company
+        this.departure = []
+        this.arrival = []
+    }
+    addDeparture(Depart) {
+        this.departure.push(Depart)
+    }
+    addArrival(Arrive) {
+        this.arrival.push(Arrive)
     }
 }
 
 P1 = new Aircraft("Boeing 737", "73G", 188, "British Airways")
-console.log(P1.model)
+const P1Depart = new Airport("London Heathrow", "England", "LHR")
+const P1Arrival = new Airport("Leeds Bradford Airport", "England", "EGNM")
+P1.addDeparture(P1Depart)
+P1.addArrival(P1Arrival)
+console.log(P1)
 
 module.exports = {Aircraft}
---
+++ Week1/AirportSystem/CrewMember.js
@@ -0,0 +1,11 @@
+class Crew {
+    constructor(name, role, staffID){
+        this.name = name
+        this.role = role
+        this.staffID = staffID
+    }
+
+}
+
+const crew1 = new Crew("Maverick Flyman", "Pilot", "ABC123")
+console.log(crew1)
