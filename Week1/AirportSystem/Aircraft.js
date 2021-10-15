d78fd407b8a44f80b08d300e7bb6a0c3bfc20068
BluFox2003 <matthew.reynard03@outlook.com>
2021-10-15 11:24:30 +0100

    yes

--- Week1/AirportSystem/Aircraft.js
+++ Week1/AirportSystem/Aircraft.js
@@ -1,6 +1,7 @@
 const {Airport} = require("./Airport")
 
 class Aircraft {
+    static planes = []
     constructor(model, registration, capacity, company){
         this.model = model
         this.registration = registration
@@ -8,6 +9,8 @@ class Aircraft {
         this.company = company
         this.departure = []
         this.arrival = []
+        this.constructor.planes.push(this)
+
     }
     addDeparture(Depart) {
         this.departure.push(Depart)
@@ -15,13 +18,23 @@ class Aircraft {
     addArrival(Arrive) {
         this.arrival.push(Arrive)
     }
+    static flyPlane(Depart, Arrive) {
+        return
+    }
 }
 
 P1 = new Aircraft("Boeing 737", "73G", 188, "British Airways")
+P2 = new Aircraft("Boeing F/A-18", "ABC12", 2, "US Navy")
 const P1Depart = new Airport("London Heathrow", "England", "LHR")
 const P1Arrival = new Airport("Leeds Bradford Airport", "England", "EGNM")
+const P2Depart = new Airport("London Heathrow", "England", "LHR")
+const P2Arrival = new Airport("Leeds Bradford Airport", "England", "EGNM")
 P1.addDeparture(P1Depart)
 P1.addArrival(P1Arrival)
-console.log(P1)
+P2.addDeparture(P2Depart)
+P2.addArrival(P2Arrival)
+
+console.log(Aircraft.planes[0].departure)
+
 
 module.exports = {Aircraft}
