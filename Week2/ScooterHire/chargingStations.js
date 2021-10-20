304a0e5c9d70054f25dcbead3aaf6d90cdd14c99
BluFox2003 <matthew.reynard03@outlook.com>
2021-10-20 13:51:48 +0100

    Added charge method

--- Week2/ScooterHire/chargingStations.js
+++ Week2/ScooterHire/chargingStations.js
@@ -4,7 +4,6 @@ class chargingStation {
   constructor(CSID) {
     this.CSID = CSID;
     this.scooters = Scooter.scooters;
-    //this.scooters.push(Scooter.scooters);
     this.scooterAmount = this.scooters.length;
     //this.constructor.chargingStations.push(this);
   }
@@ -12,18 +11,27 @@ class chargingStation {
     this.rentedScooter = this.scooters.shift();
     return this.rentedScooter;
   }
+  async chargeScooter() {
+    console.log("Charging " + this.rentedScooter.scooterID);
+    await new Promise((resolve) => setTimeout(resolve, 20000));
+    console.log("Scooter charged and ready to use :)");
+  }
   returnScooter() {
+    this.chargeScooter();
+    console.log(
+      "\n Thank you for using the scooters, your account has been charged £15 \n "
+    );
     this.scooters.push(this.rentedScooter);
     this.rentedScooter = "";
   }
 }
 
-//const CS1 = new chargingStation("CS1");
+const CS1 = new chargingStation("CS1");
 
-//console.log(CS1.scooterAmount);
-// console.log(CS1.rentScooter());
+console.log(CS1.rentScooter());
 // console.log(CS1.scooters);
-// CS1.returnScooter();
+CS1.returnScooter();
+
 // console.log(CS1.scooters);
 // console.log(CS1.rentedScooter);
 
