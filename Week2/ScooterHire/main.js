c0cf1ebf8b2729f7dfe91ce20d94a1aac232e767
BluFox2003 <matthew.reynard03@outlook.com>
2021-10-21 10:25:21 +0100

    Finished main program

--- Week2/ScooterHire/main.js
+++ Week2/ScooterHire/main.js
@@ -13,23 +13,25 @@ class Main {
   }
 }
 Customer = new User(userName, Number(userAge), userPassword);
-let A = User.isUser18(Customer);
+
 const answer1 = prompt(
   `\n Hello ${userName}, Would you like to rent a scooter? (Yes or No) `
 );
-if (answer1 == "Yes" || "yes" || "Y" || "y") {
-  if (A == true) {
+
+if (answer1 == "Yes" || answer1 == "yes" || answer1 == "Y" || answer1 == "y") {
+  if (User.isUser18(Customer) == true) {
     CS1.rentScooter();
     console.log(
       "\n Please return the scooter after 4 hours or before it runs out of charge \n "
     );
-    Main.waitTime();
-    console.log("Time to return the scooter");
-    CS1.returnScooter();
+    Main.waitTime().then(() => {
+      console.log("\nTime to return the scooter\n");
+      CS1.returnScooter();
+    });
   } else {
     console.log("Goodbye");
   }
 } else {
-  console.log("Program will end");
-  process.abort();
+  console.log("\nProgram will end");
+  //process.abort();
 }
