b0b7538f7aa058a1d110eb7182aa8844ae95610c
BluFox2003 <matthew.reynard03@outlook.com>
2021-11-01 11:15:49 +0000

    Server stuff

---
+++ Week4/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const port = 3001;
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`server listening on ${port}`);
+});
