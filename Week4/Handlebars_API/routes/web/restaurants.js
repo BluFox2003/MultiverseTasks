e7b53b3ee6405a126e2eca183b6e065f6296bbe2
BluFox2003 <matthew.reynard03@outlook.com>
2021-11-05 10:59:42 +0000

    AHHHHHH

--- Week4/Handlebars_API/routes/web/restaurants.js
+++ Week4/Handlebars_API/routes/web/restaurants.js
@@ -31,13 +31,30 @@ Router.delete("/:id/delete", async (req, res, next) => {
     const response = await fetch(url + `/${req.params.id}`, {
       method: "DELETE",
     });
-    console.log("test3");
-    // if (response.ok) {
-    // res.redirect(console.log("test"));
     res.redirect("/restaurants");
     // }
   } catch (error) {
     console.log("test2");
+    return next(error);
+  }
+});
+
+Router.get("/:id", async (req, res, next) => {
+  if (req.params.id === "create") {
+    res.render("createRestaurant");
+  }
+});
+Router.post("/create", async (req, res, next) => {
+  try {
+    await fetch(url, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(req.body), // data comes from a form
+    });
+    res.redirect("http://localhost:8002/restaurants");
+  } catch (error) {
     return next(error);
   }
 });
